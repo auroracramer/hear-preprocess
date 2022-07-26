64e25e48eaba6da49319bdbe9b2ad413c2494257
Aurora Cramer <aurora.linh.cramer@gmail.com>
2022-07-25 22:03:11 -0400

    Add TAU 2021 SSE NIGENS task for xy projection

---
+++ hearpreprocess/soundata_tau2021sse_nigens_xy.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import copy
+import logging
+import hearpreprocess.soundata_tau2021sse_nigens as soundata_tau2021sse_nigens
+
+logger = logging.getLogger("luigi-interface")
+
+# Copy the regular TAU 2021 SSE NIGENS config - updated here for targets
+generic_task_config = copy.deepcopy(soundata_tau2021sse_nigens.generic_task_config)
+
+generic_task_config.update({
+    "task_name": "soundata_tau2021sse_nigens_xy",
+    "spatial_projection": "unit_xy_disc", # none, unit_sphere, unit_xy_disc, unit_yz_disc
+})
